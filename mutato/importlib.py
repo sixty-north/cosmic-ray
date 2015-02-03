2581cf15aa894fece1d50ef6e43d7d1cecd67bf9
Austin Bingham <austin.bingham@gmail.com>
2015-02-03 08:20:23 +0100

    Beefed up the import utility code.

--- mutato/importlib.py
+++ mutato/importlib.py
@@ -1,17 +1,20 @@
+"""Functionality related to Python's import mechanisms.
+"""
+
+from itertools.import islice
+import sys
+
 from importlib.abc import MetaPathFinder
 from importlib.machinery import ModuleSpec
 
 
 class ASTLoader:
-    def __init__(self, ast):
+    def __init__(self, ast, name):
         self._ast = ast
-
-    @property
-    def source_code(self):
-        return generate_source_code(self._ast)
+        self._name = name
 
     def exec_module(self, mod):
-        exec(self.source_code,
+        exec(compile(self._ast, self.name, 'exec'),
              mod.__dict__)
 
 
@@ -23,4 +26,35 @@ class Finder(MetaPathFinder, dict):
     def find_spec(self, fullname, path, target=None):
         try:
             return ModuleSpec(fullname,
-                              ASTLoader(self[fullname]))
+                              ASTLoader(self[fullname], fullname))
+        except KeyError:
+            pass
+
+
+def enable_ast_loading():
+    """Ensure that a `Finder` is installed on `sys.meta_path`.
+
+    This has the effect of allowing imports via any AST tree managed
+    by the `Finder`.
+
+    If no `Finder` instance is found in `sys.meta_path`, a new one is
+    created, prepended to `sys.meta_path`, and returned.
+
+    If there are one more more `Finder` instances in `sys.meta_path`,
+    the first on in the list is returned.
+    """
+    try:
+        finder = [f for f in sys.meta_path if isinstance(f, Finder)][0]
+    except IndexError:
+        finder = Finder()
+        sys.meta_path = [finder] + sys.meta_path
+
+    return finder
+
+
+def disable_ast_loading():
+    """Remove all instances of `Finder` from `sys.meta_path`.
+
+    This has the effect of preventing importing via AST nodes.
+    """
+    sys.meta_path = [f for f in sys.meta_path if not isinstance(f, Finder)]
