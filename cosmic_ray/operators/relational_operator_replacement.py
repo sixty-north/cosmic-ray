78f70b496228c74753cb623a96019ac96333e0ff
Austin Bingham <austin.bingham@gmail.com>
2015-03-13 09:02:10 +0100

    rel-op replacers are now added to their module object.
    Without doing this, they can't be properly sent across process
    boundaries e.g. for use with multiprocessing.

--- cosmic_ray/operators/relational_operator_replacement.py
+++ cosmic_ray/operators/relational_operator_replacement.py
@@ -9,8 +9,6 @@ RELATIONAL_OPERATORS = {ast.Eq, ast.NotEq, ast.Lt, ast.LtE, ast.Gt, ast.GtE,
 # For each relational operator A, create one class for each *other*
 # relational operator B which replaces A with B in an AST.
 
-operators = []
-
 for from_op in RELATIONAL_OPERATORS:
     for to_op in RELATIONAL_OPERATORS.difference({from_op}):
         operator_name = 'Replace{}With{}'.format(
@@ -19,11 +17,11 @@ for from_op in RELATIONAL_OPERATORS:
         visit_func_name = 'visit_{}'.format(from_op.__name__)
         visit_func = lambda self, node: self.visit_mutation_site(node)
 
-        operators.append(
-            type(operator_name,
-                 (Operator,),
-                 {'mutate': lambda self, node: to_op(),
-                  visit_func_name: visit_func,
-                  'from_op': from_op,
-                  '__repr__': lambda self: 'replace {} with {}'.format(
-                      from_op.__name__, to_op.__name__)}))
+        globals()[operator_name] = type(
+            operator_name,
+            (Operator,),
+            {'mutate': lambda self, node: to_op(),
+             visit_func_name: visit_func,
+             'from_op': from_op,
+             '__repr__': lambda self: 'replace {} with {}'.format(
+                 from_op.__name__, to_op.__name__)})
