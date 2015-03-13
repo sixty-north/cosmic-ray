400c3b582638e5f21350f2a6949a09b67839ec87
Austin Bingham <austin.bingham@gmail.com>
2015-03-13 10:52:09 +0100

    Cleanup and test fixes.

--- cosmic_ray/operators/relational_operator_replacement.py
+++ cosmic_ray/operators/relational_operator_replacement.py
@@ -6,6 +6,8 @@ from .operator import Operator
 RELATIONAL_OPERATORS = {ast.Eq, ast.NotEq, ast.Lt, ast.LtE, ast.Gt, ast.GtE,
                         ast.Is, ast.IsNot, ast.In, ast.NotIn}
 
+operators = []
+
 # For each relational operator A, create one class for each *other*
 # relational operator B which replaces A with B in an AST.
 
@@ -17,7 +19,7 @@ for from_op in RELATIONAL_OPERATORS:
         visit_func_name = 'visit_{}'.format(from_op.__name__)
         visit_func = lambda self, node: self.visit_mutation_site(node)
 
-        globals()[operator_name] = type(
+        new_op = type(
             operator_name,
             (Operator,),
             {'mutate': lambda self, node: to_op(),
@@ -25,3 +27,6 @@ for from_op in RELATIONAL_OPERATORS:
              'from_op': from_op,
              '__repr__': lambda self: 'replace {} with {}'.format(
                  from_op.__name__, to_op.__name__)})
+
+        globals()[operator_name] = new_op
+        operators.append(new_op)
