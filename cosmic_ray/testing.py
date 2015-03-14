43e438d84df93c31cf4aebceb33c82e524cd4e7e
Austin Bingham <austin.bingham@gmail.com>
2015-03-14 15:29:36 +0100

    Fixed bug where we incorrectly reported test failure.

--- cosmic_ray/testing.py
+++ cosmic_ray/testing.py
@@ -10,4 +10,4 @@ def run_tests(test_dir):
     suite = unittest.TestLoader().discover(test_dir)
     result = unittest.TestResult()
     suite.run(result)
-    return result.wasSuccessful, str(result)
+    return result.wasSuccessful(), str(result)
