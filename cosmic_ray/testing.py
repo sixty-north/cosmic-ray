5fab7ab1f182347f115222dd6f28ce410c59f81f
Austin Bingham <austin.bingham@gmail.com>
2015-03-14 15:43:04 +0100

    Returning structured data from unittest runs.

--- cosmic_ray/testing.py
+++ cosmic_ray/testing.py
@@ -1,3 +1,4 @@
+from itertools import chain
 import unittest
 
 
@@ -10,4 +11,7 @@ def run_tests(test_dir):
     suite = unittest.TestLoader().discover(test_dir)
     result = unittest.TestResult()
     suite.run(result)
-    return result.wasSuccessful(), str(result)
+    return (result.wasSuccessful(),
+            [(str(r[0]), r[1])
+             for r in chain(result.errors,
+                            result.failures)])
