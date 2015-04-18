81a4077d866336483a5f8e4c9562415514f46e19
Austin Bingham <austin.bingham@gmail.com>
2015-04-18 13:11:53 +0200

    Removed vestigial comment.

--- test_project/tests/test_adam.py
+++ test_project/tests/test_adam.py
@@ -6,10 +6,6 @@ import adam
 
 
 class Tests(unittest.TestCase):
-    # TODO: Without this test, the adam tests should have a
-    # survivor. However, all are killed. It appears that modules are
-    # still not being replaced properly. Ugh.s
-
     def test_constant_number(self):
         self.assertEqual(
             adam.constant_number(),
