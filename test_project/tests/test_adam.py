bd0fbb24fa5e714236b6dc13cdbea5f7cbe1b3b8
Austin Bingham <austin.bingham@gmail.com>
2015-04-18 13:13:37 +0200

    Disabled some adam tests to test travis.
    I'm doing to the make sure travis will report failure correctly. I'll
    undo this change shortly.

--- test_project/tests/test_adam.py
+++ test_project/tests/test_adam.py
@@ -6,20 +6,20 @@ import adam
 
 
 class Tests(unittest.TestCase):
-    def test_constant_number(self):
-        self.assertEqual(
-            adam.constant_number(),
-            42)
+    # def test_constant_number(self):
+    #     self.assertEqual(
+    #         adam.constant_number(),
+    #         42)
 
-    def test_unary_sub(self):
-        self.assertEqual(
-            adam.unary_sub(),
-            -1)
+    # def test_unary_sub(self):
+    #     self.assertEqual(
+    #         adam.unary_sub(),
+    #         -1)
 
-    def test_unary_add(self):
-        self.assertEqual(
-            adam.unary_add(),
-            +1)
+    # def test_unary_add(self):
+    #     self.assertEqual(
+    #         adam.unary_add(),
+    #         +1)
 
     def test_equals(self):
         vals = [uuid.uuid1(),
