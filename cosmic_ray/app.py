0595ce9fc16776a769dfd13f254eb2188c86d37a
Austin Bingham <austin.bingham@gmail.com>
2015-04-26 22:32:00 +0200

    Documentation and cleanup.

--- cosmic_ray/app.py
+++ cosmic_ray/app.py
@@ -31,12 +31,17 @@ def format_test_result(mutation_record, test_result):
         reason=test_result.results)
 
 
-class MutantQueue(pykka.ThreadingActor):
+class QueueManager(pykka.ThreadingActor):
+    """An actor that distributes the mutation records to testers.
+    """
     def __init__(self, mutation_records):
         super().__init__()
         self._record_iterator = iter(mutation_records)
 
     def next(self):
+        """Return the next mutation record, or `None` if there are no more
+        records.
+        """
         try:
             return next(self._record_iterator)
         except StopIteration:
@@ -44,11 +49,17 @@ class MutantQueue(pykka.ThreadingActor):
 
 
 class Logger(pykka.ThreadingActor):
+    """A simple logging actor.
+
+    This received test results and just writes them to the console.
+    """
     def handle_result(self, mutation_record, test_result):
         print(format_test_result(mutation_record, test_result))
 
 
 class Summarizer(pykka.ThreadingActor):
+    """An actor that maintains simple statistics on the testing outcomes.
+    """
     def __init__(self):
         super().__init__()
         self.outcomes = {o: 0 for o in Outcome}
@@ -58,6 +69,8 @@ class Summarizer(pykka.ThreadingActor):
 
 
 class MutantTester(pykka.ThreadingActor):
+    """An actor that executes tests on mutants.
+    """
     def __init__(self, test_runner, timeout, *handlers):
         super().__init__()
         self._test_runner = test_runner
@@ -73,6 +86,12 @@ class MutantTester(pykka.ThreadingActor):
             self._process_queue(msg['process_queue'])
 
     def _process_queue(self, queue):
+        """Fetch the next mutation records from the queue and test it.
+
+        If there are no more records, this calls
+        `self.stop`. Otherwise, after testing the mutant, this sends a
+        message to itself to process another mutant.
+        """
         record = queue.next().get()
 
         if record is None:
@@ -135,7 +154,7 @@ def main():
 
     LOG.info('Creating actors')
 
-    queue = MutantQueue.start(mutation_records).proxy()
+    queue = QueueManager.start(mutation_records).proxy()
     logger = Logger.start().proxy()
     summarizer = Summarizer.start().proxy()
     testers = [MutantTester.start(test_runner, timeout, logger, summarizer)
@@ -153,27 +172,31 @@ def main():
     LOG.info('created event loop')
 
     def check_testers():
+        """Check to see if any testers are still alive, terminating the event
+        loop if not.
+        """
         if any([t.is_alive() for t in testers]):
             loop.call_soon(check_testers)
         else:
             LOG.info('stopping loopg...')
             loop.stop()
 
-    # Schedule a call to hello_world()
+    # Schedule a call to check_testers
     loop.call_soon(check_testers)
 
     LOG.info('scheduled test checking')
 
-    # Blocking call interrupted by loop.stop()
-    LOG.info('running forever...')
+    LOG.info('Starting event loop')
     loop.run_forever()
-    LOG.info('not running forever...')
+    LOG.info('Closing the event loop')
     loop.close()
-    LOG.info('loop closed...')
+    LOG.info('Event loop closed')
 
+    LOG.info('Stopping actors')
     queue.stop()
     logger.stop()
     summarizer.stop()
+    LOG.info('Actors stopped')
 
     outcomes = summarizer.outcomes.get()
     total_count = sum(outcomes.values())
