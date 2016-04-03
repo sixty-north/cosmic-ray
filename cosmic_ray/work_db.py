2e7d26f61894cd8704cfd076851fd0272efd02d2
Austin Bingham <austin@sixty-north.com>
2016-04-03 21:31:16 +0200

    Tightened up work-db implementation.

--- cosmic_ray/work_db.py
+++ cosmic_ray/work_db.py
@@ -20,12 +20,38 @@
 #    test_runner.TestResult.
 
 import contextlib
+from enum import Enum
+import os
 
+# This db may well not scale very well. We need to be ready to switch it out
+# for something quicker if not. But for now it's *very* convenient.
 import tinydb
 
 
 class WorkDB:
-    def __init__(self, path):
+    class Mode(Enum):
+        # Open existing files, creating if necessary
+        create = 1,
+
+        # Open only existing files, failing if it doesn't exist
+        open = 2
+
+    def __init__(self, path, mode):
+        """Open a DB in file `path` in mode `mode`.
+
+        Args:
+          path: The path to the DB file.
+          mode: The mode in which to open the DB. See the `Mode` enum for
+            details.
+
+        Raises:
+          FileNotFoundError: If `mode` is `Mode.open` and `path` does not
+            exist.
+        """
+        if (mode == WorkDB.Mode.open) and (not os.path.exists(path)):
+            raise FileNotFoundError(
+                'Requested file {} not found'.format(path))
+
         self._db = tinydb.TinyDB(path)
 
     def close(self):
@@ -33,13 +59,24 @@ class WorkDB:
 
     @property
     def _work_parameters(self):
+        """The table of work parameters.
+        """
         return self._db.table('work-parameters')
 
     @property
     def _work_items(self):
+        """The table of work items.
+        """
         return self._db.table('work-items')
 
     def set_work_parameters(self, test_runner, test_directory, timeout):
+        """Set (replace) the work parameters for the session.
+
+        Args:
+          test_runner: The name of the test runner plugin to use.
+          test_directory: The directory containing tests to run.
+          timeout: The timeout for tests.
+        """
         table = self._work_parameters
         table.purge()
         table.insert({
@@ -49,6 +86,13 @@ class WorkDB:
         })
 
     def get_work_parameters(self):
+        """Get the work parameters (if set) for the session.
+
+        Returns: a tuple of `(test-runner, test-directory, timeout)`.
+
+        Raises:
+          ValueError: If there are no work parameters set for the session.
+        """
         table = self._work_parameters
 
         try:
@@ -61,6 +105,16 @@ class WorkDB:
                 record['timeout'])
 
     def add_work_items(self, items):
+        """Add a sequence of new work items to the session.
+
+        These are added with no associated results and will be considered
+        "pending".
+
+        Args:
+          items: An iterable of tuples of the form `(module-name,
+            operator-name, occurrence)`.
+
+        """
         table = self._work_items
         table.insert_multiple(
             {'module-name': i[0],
@@ -69,13 +123,36 @@ class WorkDB:
             for i in items)
 
     def clear_work_items(self):
+        """Clear all work items from the session.
+
+        This removes any associated results as well.
+        """
         self._work_items.purge()
 
     @property
     def work_items(self):
+        """The sequence of work items in the session.
+
+        This include both complete and incomplete items.
+
+        Each work item is a dict with the keys `module-name`, `op-name`, and
+        `occurrence`. Items with results will also have the keys `results-type`
+        and `results-data`.
+
+        """
         return self._work_items.all()
 
     def add_results(self, job_id, results_type, results_data):
+        """Add a result to the session for the work-item with id `job_id`.
+
+        Args:
+          job_id: The ID of the work-item to update.
+          results_type: One of 'exception', 'no-test', or 'normal'.
+          results_data: The associated data, if any.
+
+        Raises:
+          KeyError: If there is no work-item with the id `job_id`.
+        """
         table = self._work_items
         table.update(
             {
@@ -86,6 +163,12 @@ class WorkDB:
 
     @property
     def pending_work(self):
+        """The sequence of pending work in the session.
+
+        Each item is a tuple of the form `(job-id, module-name, operator-name,
+        occurrence)`.
+
+        """
         table = self._work_items
         work_item = tinydb.Query()
 
@@ -98,7 +181,21 @@ class WorkDB:
 
 
 @contextlib.contextmanager
-def use_db(path):
+def use_db(path, mode=WorkDB.Mode.create):
+    """Open a DB in file `path` in mode `mode` as a context manager.
+
+    On exiting the context the DB will be automatically closed.
+
+    Args:
+      path: The path to the DB file.
+      mode: The mode in which to open the DB. See the `Mode` enum for
+        details.
+
+    Raises:
+      FileNotFoundError: If `mode` is `Mode.open` and `path` does not
+        exist.
+    """
+
     db = WorkDB(path)
     try:
         yield db
