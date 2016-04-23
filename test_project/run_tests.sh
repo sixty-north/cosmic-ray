0d7877df3285b52f0ce4d42da8a4afde98671808
Austin Bingham <austin@sixty-north.com>
2016-04-23 16:59:49 +0100

    Improved test runner script.

--- test_project/run_tests.sh
+++ test_project/run_tests.sh
@@ -1,6 +1,8 @@
 # This runs the "adam" tests, returning 0 if all mutants are killed, or 1 if
 # there's a survivor.
 
+set -e
+
 cosmic-ray load cosmic-ray.unittest.conf
 RESULT=`cosmic-ray survival-rate adam_tests.unittest`
 if [ $RESULT != 0.00 ]; then exit 1; fi
