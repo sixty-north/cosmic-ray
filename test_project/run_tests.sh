0abde8e2e94256da13dc6c31f529d48bc953eee6
Austin Bingham <austin@sixty-north.com>
2016-04-23 18:30:12 +0100

    Fixed up test runner script

--- test_project/run_tests.sh
+++ test_project/run_tests.sh
@@ -4,15 +4,15 @@
 set -e
 
 cosmic-ray load cosmic-ray.unittest.conf
-cosmic-ray report adam_tests.unittest
-cosmic-ray survival-rate adam_tests.unittest
-#RESULT=`cosmic-ray survival-rate adam_tests.unittest`
-#if [ $RESULT != 0.00 ]; then exit 1; fi
+#cosmic-ray report adam_tests.unittest
+#cosmic-ray survival-rate adam_tests.unittest
+RESULT=`cosmic-ray survival-rate adam_tests.unittest`
+if [ $RESULT != 0.00 ]; then exit 1; fi
 
 cosmic-ray load cosmic-ray.pytest.conf
-cosmic-ray report adam_tests.pytest
-cosmic-ray survival-rate adam_tests.pytest
-#RESULT=`cosmic-ray survival-rate adam_tests.pytest`
-#if [ $RESULT != 0.00 ]; then exit 1; fi
+#cosmic-ray report adam_tests.pytest
+#cosmic-ray survival-rate adam_tests.pytest
+RESULT=`cosmic-ray survival-rate adam_tests.pytest`
+if [ $RESULT != 0.00 ]; then exit 1; fi
 
 exit 0
