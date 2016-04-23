adc051bb2aaad86f4feea505ee4cca00d4c34a03
Austin Bingham <austin@sixty-north.com>
2016-04-23 12:19:57 +0100

    Using pytest for operator tests.

--- cosmic_ray/test/test_get_line_number.py
+++ cosmic_ray/test/test_get_line_number.py
@@ -1,26 +1,14 @@
 import ast
-import unittest
 
 from cosmic_ray.util import get_line_number
 
 
-class Tests(unittest.TestCase):
-    def test_basic(self):
-        code = 'if x == 1:' \
-               '    return 3'
-        tree = ast.parse(code)
-        self.assertEqual(
-            get_line_number(tree),
-            '<UNKNOWN>')
-        self.assertEqual(
-            get_line_number(
-                tree.body[0]),
-            1)
-        self.assertEqual(
-            get_line_number(
-                tree.body[0].test),
-            1)
-        self.assertEqual(
-            get_line_number(
-                tree.body[0].test.ops[0]),
-            '<UNKNOWN>')
+def test_basic():
+    code = 'if x == 1:' \
+           '    return 3'
+    tree = ast.parse(code)
+
+    assert get_line_number(tree) == '<UNKNOWN>'
+    assert get_line_number(tree.body[0]) == 1
+    assert get_line_number(tree.body[0].test) == 1
+    assert get_line_number(tree.body[0].test.ops[0]) == '<UNKNOWN>'
