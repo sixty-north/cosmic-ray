7556d381a43755bf435c9957ab73c1b888020030
Austin Bingham <austin@sixty-north.com>
2016-12-19 05:39:42 +0100

    Updated run_test.sh with new dist/local split.

--- test_project/run_tests.sh
+++ test_project/run_tests.sh
@@ -1,55 +1,20 @@
 # This runs the "adam" tests, returning 0 if all mutants are killed, or 1 if
 # there's a survivor.
 
-cosmic-ray load cosmic-ray.unittest.dist.conf
-if [ $? != 0 ]; then exit 1; fi
-RESULT=`cosmic-ray survival-rate adam_tests.unittest.dist`
-if [ $RESULT != 0.00 ]; then
-    cosmic-ray report adam_tests.unittest.dist
-    exit 1
-fi
+TEST_CONFIGS="unittest.dist unittest.local pytest.dist pytest.local nosetest.dist nosetest.dist"
+for CONFIG in $TEST_CONFIGS; do
+    echo $CONFIG
+    cosmic-ray load cosmic-ray.$CONFIG.conf
+    if [ $? != 0 ]; then exit 1; fi
+    RESULT=`cosmic-ray survival-rate adam_tests.$CONFIG`
+    if [ $RESULT != 0.00 ]; then
+        cosmic-ray report adam_tests.$CONFIG
+        exit 1
+    fi
+done
 
-cosmic-ray load cosmic-ray.unittest.local.conf
-if [ $? != 0 ]; then exit 1; fi
-RESULT=`cosmic-ray survival-rate adam_tests.unittest.local`
-if [ $RESULT != 0.00 ]; then
-    cosmic-ray report adam_tests.unittest.local
-    exit 1
-fi
-
-cosmic-ray load cosmic-ray.pytest.dist.conf
-if [ $? != 0 ]; then exit 1; fi
-RESULT=`cosmic-ray survival-rate adam_tests.pytest.dist`
-if [ $RESULT != 0.00 ]; then
-    cosmic-ray report adam_tests.pytest.dist
-    exit 1
-fi
-
-cosmic-ray load cosmic-ray.pytest.local.conf
-if [ $? != 0 ]; then exit 1; fi
-RESULT=`cosmic-ray survival-rate adam_tests.pytest.local`
-if [ $RESULT != 0.00 ]; then
-    cosmic-ray report adam_tests.pytest.local
-    exit 1
-fi
-
-cosmic-ray load cosmic-ray.nosetest.dist.conf
-if [ $? != 0 ]; then exit 1; fi
-RESULT=`cosmic-ray survival-rate adam_tests.nosetest`
-if [ $RESULT != 0.00 ]; then
-    cosmic-ray report adam_tests.nosetest.dist
-    exit 1
-fi
-
-cosmic-ray load cosmic-ray.nosetest.local.conf
-if [ $? != 0 ]; then exit 1; fi
-RESULT=`cosmic-ray survival-rate adam_tests.nosetest`
-if [ $RESULT != 0.00 ]; then
-    cosmic-ray report adam_tests.nosetest.local
-    exit 1
-fi
-
-# Run import tests
+# #
+Run import tests
 cosmic-ray load cosmic-ray.import.conf
 if [ $? != 0 ]; then exit 1; fi
 RESULT=`cosmic-ray survival-rate import_tests`
