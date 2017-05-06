ad02b8c6abad3709c62ef613ad2696fa99f63556
Austin Bingham <austin@sixty-north.com>
2017-05-06 12:24:34 +0200

    Updated the make_record docs and API a bit.

--- cosmic_ray/work_record.py
+++ cosmic_ray/work_record.py
@@ -1,4 +1,25 @@
-def make_record(name, *fields):
+def make_record(name, fields=(), docstring=""):
+    """Create a new record class.
+
+    A Record is fundamentally a dict with a specified set of keys. These keys
+    will always have a value (defaulting to None), they can't be removed, and
+    new keys can not be added.
+
+    This may sound a lot like a class, and that's true. The main benefit of
+    records is that they can be treated directly like dicts for the most part,
+    and, critically, they are easy to JSON-ify. Also, like classes, they ensure
+    that they're only used in the correct way, i.e. users can only access the
+    specified fields. This prevents the confusion of using simple dicts where
+    people can use conflicting or confusing key names.
+
+    Args:
+        name: The name of the class to be created.
+        fields: The names of the fields in the record.
+        docstring: The docstring for the record class.
+
+    Returns: A new class derived from dict with the specified fields.
+
+    """
     def __init__(self, vals=None, **kwargs):
         dict.__init__(self, dict.fromkeys(fields))
         vals = vals or dict()
@@ -46,37 +67,41 @@ def make_record(name, *fields):
     }
 
     rec = type(name, (dict,), attrs)
+    rec.__doc__ = docstring
     return rec
 
 
 WorkRecord = make_record(
     'WorkRecord',
 
-    # Arbitrary data returned by the concrete TestRunner to provide more
-    # information about the test results.
-    'data',
+    [
+        # Arbitrary data returned by the concrete TestRunner to provide more
+        # information about the test results.
+        'data',
 
-    # A test_runner.TestOutcome from the test run.
-    'test_outcome',
+        # A test_runner.TestOutcome from the test run.
+        'test_outcome',
 
-    # A worker.WorkOutcome describing how the worker completed.
-    'worker_outcome',
+        # A worker.WorkOutcome describing how the worker completed.
+        'worker_outcome',
 
-    # The diff produced by the operators
-    'diff',
+        # The diff produced by the operators
+        'diff',
 
-    # the module to be mutated
-    'module',
+        # the module to be mutated
+        'module',
 
-    # The name of the operators
-    'operator',
+        # The name of the operators
+        'operator',
 
-    # The occurrence on which the operator was applied.
-    'occurrence',
+        # The occurrence on which the operator was applied.
+        'occurrence',
 
-    # The line number at which the operator was applied.
-    'line_number',
+        # The line number at which the operator was applied.
+        'line_number',
 
-    'command_line',
-    'job_id'
-)
+        'command_line',
+        'job_id'
+    ],
+    docstring=" The details of a specific mutation and test run in CosmicRay."
+    )
--- test/test_record.py
+++ test/test_record.py
@@ -21,7 +21,7 @@ def test_empty_constructor_has_no_keys():
 
 @given(ST.sets(attributes))
 def test_default_attr_value_is_none(attrs):
-    r = make_record('Rec', *attrs)()
+    r = make_record('Rec', attrs)()
     for attr in attrs:
         assert getattr(r, attr) is None
     assert len(r) == len(attrs)
@@ -29,7 +29,7 @@ def test_default_attr_value_is_none(attrs):
 
 @given(attributes, ST.text())
 def test_can_assign_to_field_attributes(attr, value):
-    r = make_record('Rec', attr)()
+    r = make_record('Rec', [attr])()
     setattr(r, attr, value)
     assert getattr(r, attr) == value
 
@@ -37,14 +37,14 @@ def test_can_assign_to_field_attributes(attr, value):
 @given(attributes, attributes)
 def test_cannot_assign_to_non_field_attributes(attr, nonattr):
     assume(attr != nonattr)
-    r = make_record('Rec', attr)()
+    r = make_record('Rec', [attr])()
     with pytest.raises(AttributeError):
         r.bar = 'llama'
 
 
 @given(attributes)
 def test_can_index_fields(attr):
-    r = make_record('Rec', attr)()
+    r = make_record('Rec', [attr])()
     r[attr] = 42
     assert r[attr] == 42
 
@@ -52,7 +52,7 @@ def test_can_index_fields(attr):
 @given(attributes, attributes)
 def test_cannot_index_non_fields(attr, nonattr):
     assume(attr != nonattr)
-    r = make_record('Rec', attr)()
+    r = make_record('Rec', [attr])()
     with pytest.raises(KeyError):
         r[nonattr] = 42
     with pytest.raises(KeyError):
@@ -61,7 +61,7 @@ def test_cannot_index_non_fields(attr, nonattr):
 
 @given(attributes)
 def test_can_update_fields(attr):
-    r = make_record('Rec', attr)()
+    r = make_record('Rec', [attr])()
     r.update({attr: 42})
     assert r[attr] == 42
 
@@ -69,26 +69,26 @@ def test_can_update_fields(attr):
 @given(attributes, attributes)
 def test_cannot_update_non_fields(attr, nonattr):
     assume(attr != nonattr)
-    r = make_record('Rec', attr)()
+    r = make_record('Rec', [attr])()
     with pytest.raises(KeyError):
         r.update({nonattr: 42})
 
 
 @given(attributes)
 def test_cannot_delete_fields(attr):
-    r = make_record('Rec', attr)()
+    r = make_record('Rec', [attr])()
     with pytest.raises(KeyError):
         del r[attr]
 
 
 @given(ST.sets(attributes))
 def test_can_construct_from_dict(attrs):
-    make_record('Rec', *attrs)({a: 42 for a in attrs})
+    make_record('Rec', attrs)({a: 42 for a in attrs})
 
 
 @given(attributes, attributes)
 def test_cannot_construct_from_dict_with_non_fields(attr, nonattr):
     assume(attr != nonattr)
-    R = make_record('Rec', attr)
+    R = make_record('Rec', [attr])
     with pytest.raises(KeyError):
         R({nonattr: 3})
