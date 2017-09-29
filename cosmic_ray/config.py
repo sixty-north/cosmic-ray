a0c553ebc1b39a6099f499682bfe5c985338e0fb
Austin Bingham <austin@sixty-north.com>
2017-09-29 18:12:59 +0200

    Added config.py.

---
+++ cosmic_ray/config.py
@@ -0,0 +1,21 @@
+import sys
+import yaml
+
+
+def load_config(filename=None):
+    if filename is None:
+        return yaml.load(sys.stdin)
+
+    with open(filename, mode='rt') as f:
+        return yaml.load(f)
+
+
+def get_db_name(session_name):
+    if session_name.endswith('.json'):
+        return session_name
+    else:
+        return '{}.json'.format(session_name)
+
+
+class ConfigError(Exception):
+    pass
