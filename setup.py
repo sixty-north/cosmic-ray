bbdba79c07851a61c63de3603230890f21fd9de1
Austin Bingham <austin.bingham@gmail.com>
2017-10-09 08:57:39 +0200

    Added python 3.6 to the setup tags

--- setup.py
+++ setup.py
@@ -99,6 +99,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Testing',
     ],
