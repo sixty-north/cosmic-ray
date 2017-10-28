c56b0e9baa3bfe48053f34b39a4fb65bd9bce749
Austin Bingham <austin@sixty-north.com>
2017-10-28 10:07:25 +0100

    Fixed bug in worker tests.

--- test/test_worker.py
+++ test/test_worker.py
@@ -1,4 +1,5 @@
 from cosmic_ray.operators import zero_iteration_loop
+from cosmic_ray.plugins import get_test_runner
 from cosmic_ray.work_record import WorkRecord
 from cosmic_ray.worker import worker, WorkerOutcome
 
@@ -7,8 +8,9 @@ from path_utils import DATA_DIR, excursion, extend_path
 
 def test_no_test_return_value():
     with extend_path(DATA_DIR), excursion(DATA_DIR):
+        test_runner = get_test_runner("unittest", ".")
         result = worker("a.b", zero_iteration_loop.ZeroIterationLoop,
-                        100, "pytest")
+                        100, test_runner)
         expected = WorkRecord(
             data=None,
             test_outcome=None,
