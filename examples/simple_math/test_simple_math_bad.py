27da967109b1d635d02fdf15990b68f4efcc6334
Thomas Edwards <tom.rb.edwards@gmail.com>
2017-10-30 14:31:07 +0000

    #246: Filled in the unit tests for simple_math.py

---
+++ examples/simple_math/test_simple_math_bad.py
@@ -0,0 +1,9 @@
+from .simple_math import square, cube
+
+
+def test_square():
+    assert square(3) == 9
+
+
+def test_cube():
+    assert cube(2) == 8
---
+++ examples/simple_math/test_simple_math_good.py
@@ -0,0 +1,26 @@
+from .simple_math import square, cube, mult_by_2, is_positive
+
+
+def test_square():
+    assert square(3) == 9
+
+
+def test_cube():
+    assert cube(2) == 8
+
+
+def test_mult_by_2():
+    assert mult_by_2(2) == 4
+
+
+def test_is_positive_for_positive_numbers():
+    assert is_positive(1)
+    assert is_positive(2)
+    assert is_positive(3)
+
+
+def test_is_positive_for_non_positive_numbers():
+    assert not is_positive(0)
+    assert not is_positive(-1)
+    assert not is_positive(-2)
+
