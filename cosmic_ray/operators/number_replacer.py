5b58825c52a358591c1f263810f523c5b10dd667
Austin Bingham <austin.bingham@gmail.com>
2017-11-28 14:24:25 +0100

    NumberReplacer now increments and decrements.

--- cosmic_ray/operators/number_replacer.py
+++ cosmic_ray/operators/number_replacer.py
@@ -6,14 +6,25 @@ import ast
 from .operator import Operator
 
 
+# Maps mutation count to number offsets
+OFFSET_MAP = {
+    0: 1,
+    1: -1,
+}
+
+
 class NumberReplacer(Operator):
     """An operator that modifies numeric constants."""
 
     def visit_Num(self, node):  # noqa # pylint: disable=invalid-name
         "Visit a number node."
-        return self.visit_mutation_site(node)
+        return self.visit_mutation_site(node, len(OFFSET_MAP))
 
-    def mutate(self, node, _):
+    def mutate(self, node, idx):
         """Modify the numeric value on `node`."""
-        new_node = ast.Num(n=node.n + 1)
+
+        assert idx in OFFSET_MAP, 'received count with no associated offset'
+
+        offset = OFFSET_MAP[idx]
+        new_node = ast.Num(n=node.n + offset)
         return new_node
