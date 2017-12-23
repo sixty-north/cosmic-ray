d210d4514539cb05539aad6597ee907b3707f044
Austin Bingham <austin.bingham@gmail.com>
2017-12-23 12:03:53 +0100

    Improved feedback for configuration loading.

--- cosmic_ray/config.py
+++ cosmic_ray/config.py
@@ -1,10 +1,27 @@
 """Configuration module."""
+from contextlib import contextmanager
 import logging
 import sys
 
 import yaml
 
 log = logging.getLogger()
+
+
+@contextmanager
+def _config_stream(filename):
+    """Given a configuration's filename, this returns a stream from which a configuration can be read.
+
+    If `filename` is `None` or '-' then stream will be `sys.stdin`. Otherwise,
+    it's the open file handle for the filename.
+    """
+    if filename is None or filename == '-':
+        log.info('Reading config from stdin')
+        yield sys.stdin
+    else:
+        with open(filename, mode='rt') as handle:
+            log.info('Reading config from %r', filename)
+            yield handle
 
 
 def load_config(filename=None):
@@ -17,19 +34,13 @@ def load_config(filename=None):
     Raises:
       ConfigError: If there is an error loading the config.
     """
-    if filename is None or filename == '-':
-        log.info('Reading config from stdin')
-        try:
-            return yaml.safe_load(sys.stdin)
-        except yaml.parser.ParserError as exc:
-            raise ConfigError() from exc
-
     try:
-        with open(filename, mode='rt') as handle:
-            log.info('Reading config from %r', filename)
+        with _config_stream(filename) as handle:
+            filename = handle.name
             return yaml.safe_load(handle)
     except (OSError, UnicodeDecodeError, yaml.parser.ParserError) as exc:
-        raise ConfigError() from exc
+        raise ConfigError(
+            'Error loading configuration from {}'.format(filename)) from exc
 
 
 def serialize_config(config):
