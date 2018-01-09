b3b929bb399b5e919985f3d2b9056866fbd65598
Austin Bingham <austin.bingham@gmail.com>
2018-01-09 11:20:18 +0100

    Issue 347 broken logging during plugin loading (#355)
    * Logging stevedore failures at the `error` level.
    
    The original `info` level wasn't logged because logging wasn't configured at the
    time that stevedore was loading extensions.
    
    * Added commands for listing interceptors and execution engines.
    
    * Logging all extension loading failures at the "error" level.

--- cosmic_ray/cli.py
+++ cosmic_ray/cli.py
@@ -249,6 +249,30 @@ def handle_operators(args):
 
 
 @dsc.command()
+def handle_execution_engines(args):
+    """usage: {program} execution-engines
+
+    List the available execution-engine plugins.
+    """
+    assert args
+    print('\n'.join(cosmic_ray.plugins.execution_engine_names()))
+
+    return os.EX_OK
+
+
+@dsc.command()
+def handle_interceptors(args):
+    """usage: {program} interceptors
+
+    List the available interceptor plugins.
+    """
+    assert args
+    print('\n'.join(cosmic_ray.plugins.interceptor_names()))
+
+    return os.EX_OK
+
+
+@dsc.command()
 def handle_worker(args):
     """usage: {program} worker \
     [options] <module> <operator> <occurrence> [<config-file>]
