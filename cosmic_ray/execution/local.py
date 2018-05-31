2c103f699815a7c463d5f359cafbca30134372c8
Austin Bingham <austin.bingham@gmail.com>
2018-05-31 14:53:47 +0200

    Fixed local test runner.
    it somehow lost the code that actually ran the tests!

--- cosmic_ray/execution/local.py
+++ cosmic_ray/execution/local.py
@@ -8,4 +8,5 @@ class LocalExecutionEngine(ExecutionEngine):
     "Execution engine that runs jobs on the local machine."
     def __call__(self, timeout, pending_work_items, config, on_task_complete):
         for work_item in pending_work_items:
+            work_item = worker_process(work_item, timeout, config)
             on_task_complete(work_item.job_id, work_item)
