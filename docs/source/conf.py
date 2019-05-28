8415045187359e8a71b5ffb8749f83aa073820cf
Austin Bingham <austin.bingham@gmail.com>
2019-05-28 09:26:13 +0200

    Removed _static and _template from sphinx config.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = [] # ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -91,7 +91,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = [] # ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
