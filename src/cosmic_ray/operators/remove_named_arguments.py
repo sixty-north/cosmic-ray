bd2eb0b817b9d4f9a4c7b29e4559d408e9dc5701
Alcolo47 <Alcolo47@gmail.com>
2019-09-30 14:29:23 +0200

    Adding # pragma: mutation-no-remove to avoid remove some parameters This is useful when some parameters are explicited with its default value. By expliciting some parameters, other mutation operator can operate on it.
    Typical case:
    in django ORM, fields are null=False by default, but it can be interesting to see if tests
    prevent from null=True

--- src/cosmic_ray/operators/remove_named_arguments.py
+++ src/cosmic_ray/operators/remove_named_arguments.py
@@ -9,38 +9,60 @@ Examples:
     - subprocess.Popen call
     - ORM (like django models)
 """
-
-from parso.python.tree import PythonNode, Operator as tree_Operator
+import re
+from parso.python.tree import PythonNode, Operator as tree_Operator, Newline
 from cosmic_ray.operators.operator import Operator
+from cosmic_ray.operators.util import ObjTest
 
 
 class RemoveNamedArguments(Operator):
+    re_skip = re.compile(r'.*pragma:.*mutation-no-remove.*')
+
     def mutation_positions(self, node):
-        if isinstance(node, PythonNode) and node.type == 'arglist':
+        if ObjTest(node).match(PythonNode, type='arglist'):
             for child in node.children:
-                if isinstance(child, PythonNode) and child.type == 'argument':
-                    yield child.start_pos, child.end_pos
+                if ObjTest(child).match(PythonNode, type='argument'):
+                    if not self._do_skip_argument(child):
+                        yield child.start_pos, child.end_pos
+
+    @classmethod
+    def _do_skip_argument(cls, node):
+        # Now we look if there is no # pragma: no-remove on any prefix
+        # before the next newline
+        t = ObjTest(node)
+        while t and not t.match(Newline):
+            t = t.get_next_leaf()
+            prefix = t.prefix.obj
+            if prefix:
+                return cls.re_skip.match(prefix)
+        return False
 
     def mutate(self, node, index):
         assert isinstance(node, PythonNode)
         count = 0
         for i, child in enumerate(node.children):
-            if isinstance(child, PythonNode) and child.type == 'argument':
-                if count == index:
-                    try:
-                        next_child = node.children[i + 1]
-                        if isinstance(next_child, tree_Operator) and next_child.value == ',':
-                            del node.children[i + 1]
-                    except IndexError:
-                        pass
-                    del node.children[i]
-                    return node
-                count += 1
+            if ObjTest(child).match(PythonNode, type='argument'):
+                if not self._do_skip_argument(child):
+                    if count == index:
+                        try:
+                            next_child = node.children[i + 1]
+                            if ObjTest(next_child).match(tree_Operator, value=','):
+                                del node.children[i + 1]
+                        except IndexError:
+                            pass
+                        del node.children[i]
+                        return node
+                    count += 1
 
     @classmethod
     def examples(cls):
         return (
             ("f(1, b=2)", "f(1,)"),  # Ending coma is acceptable
-            ("f(1, b=2, c=3)", "f(1, c=3)"),
-            ("f(a=1, b=2)", "f( b=2)"),
+            ("f(1, b=2, c=3)", "f(1, c=3)", 0),
+            ("f(1, b=2, c=3)", "f(1, b=2,)", 1),
+
+            ("f(a=1, b=2)", "f( b=2)", 0),
+            ("f(a=1, b=2)", "f(a=1,)", 1),
+
+            ("f(a=1, # pragma: mutation-no-remove\n  b=2)", "f(a=1,)", 0),
         )
