d417f7cc7157c506e61a61c2497b6df6a5ddd27f
Hubert Kario <hubert@kario.pl>
2019-11-02 12:41:32 +0100

    cr-rate: add support for confidence intervals from random run
    since now the tests are executed at random, we can estimate
    the survival rate for the whole population
    
    make cr-rate print the confidence interval when asked for it

--- src/cosmic_ray/tools/survival_rate.py
+++ src/cosmic_ray/tools/survival_rate.py
@@ -1,5 +1,6 @@
 "Tool for printing the survival rate in a session."
 
+import math
 import docopt
 
 from cosmic_ray.work_db import use_db, WorkDB
@@ -8,16 +9,31 @@ from cosmic_ray.work_db import use_db, WorkDB
 def format_survival_rate():
     """cr-rate
 
-    Usage: cr-rate <session-file>
+    Usage: cr-rate --estimate <session-file>
 
     Calculate the survival rate of a session.
+
+    options:
+        --estimate    Print the lower bound, estimate and upper bound of
+                      survival rate
     """
     arguments = docopt.docopt(
         format_survival_rate.__doc__, version='cr-rate 1.0')
+    show_estimate = arguments['--estimate']
+    z_score = 1.96  # for 95% confidence interval
+
     with use_db(arguments['<session-file>'], WorkDB.Mode.open) as db:
         rate = survival_rate(db)
+        num_items = db.num_work_items
+        num_complete = db.num_results
 
-    print('{:.2f}'.format(rate))
+    if show_estimate:
+        conf_int = math.sqrt(rate * (100-rate) / num_complete) \
+                   * z_score * (1 - math.sqrt(num_complete / num_items))
+        print('{:.2f} {:.2f} {:.2f}'
+              .format(rate-conf_int, rate, rate+conf_int))
+    else:
+        print('{:.2f}'.format(rate))
 
 
 def survival_rate(work_db):
