17446e27ade251a8d10265949c971a1bbde14e57
Hubert Kario <hubert@kario.pl>
2019-11-02 13:35:44 +0100

    cr-rate: add support for --fail-over
    add ability to return a non-zero exit code if the estimated failure
    rate is above the specified one, useful for CI systems

--- src/cosmic_ray/tools/survival_rate.py
+++ src/cosmic_ray/tools/survival_rate.py
@@ -1,6 +1,7 @@
 "Tool for printing the survival rate in a session."
 
 import math
+import sys
 import docopt
 
 from cosmic_ray.work_db import use_db, WorkDB
@@ -9,7 +10,7 @@ from cosmic_ray.work_db import use_db, WorkDB
 def format_survival_rate():
     """cr-rate
 
-Usage: cr-rate [--estimate] [--confidence <confidence_rate>] <session-file>
+Usage: cr-rate [--estimate] [--confidence <confidence_rate>] [--fail-over <max_value>] <session-file>
 
 Calculate the survival rate of a session.
 
@@ -19,11 +20,16 @@ options:
     --confidence <confidence_rate> Specify the confidence levels for estimates, 95 (%) by
                                    default. 80, 90, 95, 98, 99, 99.5, 99.8 and 99.9 are
                                    supported.
+    --fail-over <max_value>        Exit with a non-zero code if the survival rate is
+                                   larger than <max_value> or the calculated confidence
+                                   interval is above the <max_value> (if --estimate is used).
+                                   Specified as percentage.
 """
     arguments = docopt.docopt(
         format_survival_rate.__doc__, version='cr-rate 1.0')
     show_estimate = arguments['--estimate']
     confidence = arguments['--confidence']
+    fail_over = arguments['--fail-over']
 
     # use integers as keys as equality is not well defined for floats
     # the values are z-values for Standard Normal Probabilities
@@ -53,10 +59,16 @@ options:
     if show_estimate:
         conf_int = math.sqrt(rate * (100-rate) / num_complete) \
                    * z_score * (1 - math.sqrt(num_complete / num_items))
+        min_rate = rate - conf_int
         print('{:.2f} {:.2f} {:.2f}'
-              .format(rate-conf_int, rate, rate+conf_int))
+              .format(min_rate, rate, rate + conf_int))
+
     else:
         print('{:.2f}'.format(rate))
+        min_rate = rate
+
+    if fail_over and min_rate > float(fail_over):
+        sys.exit(1)
 
 
 def survival_rate(work_db):
