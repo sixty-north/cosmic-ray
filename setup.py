d81a612e1e4f9f8837ddfe2191700a261663f860
Travis CI on behalf of Austin Bingham <austin@sixty-north.com>
2020-01-31 14:28:49 +0100

    Bump version: 6.0.1 → 6.1.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name='cosmic_ray',
-    version='6.0.1',
+    version='6.1.0',
     packages=find_packages('src'),
     author='Sixty North AS',
     author_email='austin@sixty-north.com',
--- src/cosmic_ray/version.py
+++ src/cosmic_ray/version.py
@@ -1,4 +1,4 @@
 """Cosmic Ray version info."""
 
-__version__ = "6.0.1"
+__version__ = "6.1.0"
 __version_info__ = tuple(__version__.split('.'))
