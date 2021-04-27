29f8c9832944a552a711b9d3e112a79a1e4fe867
Travis CI on behalf of Austin Bingham <austin@sixty-north.com>
2021-04-27 21:08:24 +0200

    Deleting some cruft.

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil . (;(projectile-project-test-cmd . "pytest python/test")
-            (eval .(unless (and (boundp 'pyvenv-virtual-env-name) (equal pyvenv-virtual-env-name "cosmic-ray"))
-                       (pyvenv-workon "cosmic-ray"))))))
