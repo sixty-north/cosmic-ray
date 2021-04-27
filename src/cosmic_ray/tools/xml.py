379bc1e5cef09061cef1e4b2fc5f91a6aa1eb693
Travis CI on behalf of Austin Bingham <austin@sixty-north.com>
2021-04-27 20:50:17 +0200

    Replaced docopt with click in cr-xml.

--- src/cosmic_ray/tools/xml.py
+++ src/cosmic_ray/tools/xml.py
@@ -3,36 +3,29 @@
 import sys
 import xml.etree.ElementTree
 
-import docopt
+import click
 
-from cosmic_ray.work_db import use_db, WorkDB
+from cosmic_ray.work_db import WorkDB, use_db
 from cosmic_ray.work_item import TestOutcome, WorkerOutcome
 
 
-def report_xml():
-    """cr-xml
-
-Usage: cr-xml <session-file>
-
-Print an XML formatted report of test results for continuous integration systems
-"""
-    arguments = docopt.docopt(report_xml.__doc__, version='cr-rate 1.0')
-    with use_db(arguments['<session-file>'], WorkDB.Mode.open) as db:
+@click.command()
+@click.argument("session-file", type=click.Path(dir_okay=False, readable=True, exists=True))
+def report_xml(session_file):
+    """Print an XML formatted report of test results for continuous integration systems"""
+    with use_db(session_file, WorkDB.Mode.open) as db:
         xml_elem = _create_xml_report(db)
-        xml_elem.write(
-            sys.stdout.buffer, encoding='utf-8', xml_declaration=True)
+        xml_elem.write(sys.stdout.buffer, encoding="utf-8", xml_declaration=True)
 
 
 def _create_xml_report(db):
     errors = 0
     failed = 0
     skipped = 0
-    root_elem = xml.etree.ElementTree.Element('testsuite')
+    root_elem = xml.etree.ElementTree.Element("testsuite")
 
     for work_item, result in db.completed_work_items:
-        if result.worker_outcome in {
-                WorkerOutcome.EXCEPTION, WorkerOutcome.ABNORMAL
-        }:
+        if result.worker_outcome in {WorkerOutcome.EXCEPTION, WorkerOutcome.ABNORMAL}:
             errors += 1
         if result.is_killed:
             failed += 1
@@ -47,19 +40,19 @@ def _create_xml_report(db):
         subelement = _create_element_from_work_item(work_item)
         root_elem.append(subelement)
 
-    root_elem.set('errors', str(errors))
-    root_elem.set('failures', str(failed))
-    root_elem.set('skips', str(skipped))
-    root_elem.set('tests', str(db.num_work_items))
+    root_elem.set("errors", str(errors))
+    root_elem.set("failures", str(failed))
+    root_elem.set("skips", str(skipped))
+    root_elem.set("tests", str(db.num_work_items))
     return xml.etree.ElementTree.ElementTree(root_elem)
 
 
 def _create_element_from_work_item(work_item):
-    sub_elem = xml.etree.ElementTree.Element('testcase')
+    sub_elem = xml.etree.ElementTree.Element("testcase")
 
-    sub_elem.set('classname', work_item.job_id)
-    sub_elem.set('line', str(work_item.start_pos[0]))
-    sub_elem.set('file', str(work_item.module_path))
+    sub_elem.set("classname", work_item.job_id)
+    sub_elem.set("line", str(work_item.start_pos[0]))
+    sub_elem.set("file", str(work_item.module_path))
 
     return sub_elem
 
@@ -69,18 +62,19 @@ def _update_element_with_result(sub_elem, result):
     outcome = result.worker_outcome
 
     if outcome == WorkerOutcome.EXCEPTION:
-        error_elem = xml.etree.ElementTree.SubElement(sub_elem, 'error')
-        error_elem.set('message', "Worker has encountered exception")
+        error_elem = xml.etree.ElementTree.SubElement(sub_elem, "error")
+        error_elem.set("message", "Worker has encountered exception")
         error_elem.text = str(data) + "\n".join(result.diff)
     elif _evaluation_success(result):
-        failure_elem = xml.etree.ElementTree.SubElement(sub_elem, 'failure')
-        failure_elem.set('message', "Mutant has survived your unit tests")
+        failure_elem = xml.etree.ElementTree.SubElement(sub_elem, "failure")
+        failure_elem.set("message", "Mutant has survived your unit tests")
         failure_elem.text = str(data) + result.diff
 
     return sub_elem
 
 
 def _evaluation_success(result):
-    return result.worker_outcome == WorkerOutcome.NORMAL and \
-        result.test_outcome in {TestOutcome.SURVIVED,
-                                TestOutcome.INCOMPETENT}
+    return result.worker_outcome == WorkerOutcome.NORMAL and result.test_outcome in {
+        TestOutcome.SURVIVED,
+        TestOutcome.INCOMPETENT,
+    }
