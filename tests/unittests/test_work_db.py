de39888d167a21905c6944e05dfc84c68a89e1f9
Travis CI on behalf of Austin Bingham <austin@sixty-north.com>
2021-04-27 20:55:40 +0200

    Stopped pytest from collecting TestOutcome.

--- tests/unittests/test_work_db.py
+++ tests/unittests/test_work_db.py
@@ -4,14 +4,15 @@ import pytest
 
 from cosmic_ray.config import ConfigDict
 from cosmic_ray.work_db import use_db, WorkDB
-from cosmic_ray.work_item import TestOutcome, WorkerOutcome, WorkItem, WorkResult
+from cosmic_ray.work_item import WorkerOutcome, WorkItem, WorkResult
+from cosmic_ray.work_item import TestOutcome as TOutcome  # We do this to prevent pytest from "collecting" TOutcome
 
 # pylint: disable=W0621,C0111
 
 
 @pytest.fixture
 def work_db():
-    with use_db(':memory:', WorkDB.Mode.create) as db:
+    with use_db(":memory:", WorkDB.Mode.create) as db:
         yield db
 
 
@@ -20,86 +21,69 @@ def test_empty_db_has_no_pending_jobs(work_db):
 
 
 def test_find_pending_job(work_db):
-    item = WorkItem('path', 'operator', 0, (0, 0), (0, 1), 'job_id')
+    item = WorkItem("path", "operator", 0, (0, 0), (0, 1), "job_id")
     work_db.add_work_item(item)
     pending = list(work_db.pending_work_items)
     assert pending == [item]
 
 
 def test_jobs_with_results_are_not_pending(work_db):
-    work_db.add_work_item(
-        WorkItem('path', 'operator', 0, (0, 0), (0, 1), 'job_id'))
+    work_db.add_work_item(WorkItem("path", "operator", 0, (0, 0), (0, 1), "job_id"))
     work_db.set_result(
-        'job_id',
-        WorkResult(
-            output='data',
-            test_outcome=TestOutcome.KILLED,
-            worker_outcome=WorkerOutcome.NORMAL,
-            diff='diff'))
+        "job_id",
+        WorkResult(output="data", test_outcome=TOutcome.KILLED, worker_outcome=WorkerOutcome.NORMAL, diff="diff"),
+    )
     assert not list(work_db.pending_work_items)
 
 
 def test_set_result_throws_KeyError_if_no_matching_work_item(work_db):
     with pytest.raises(KeyError):
         work_db.set_result(
-            'job_id',
-            WorkResult(
-                output='data',
-                test_outcome=TestOutcome.KILLED,
-                worker_outcome=WorkerOutcome.NORMAL,
-                diff='diff'))
+            "job_id",
+            WorkResult(output="data", test_outcome=TOutcome.KILLED, worker_outcome=WorkerOutcome.NORMAL, diff="diff"),
+        )
 
 
 def test_set_multiple_results_works(work_db):
-    work_db.add_work_item(
-        WorkItem('path', 'operator', 0, (0, 0), (0, 1), 'job_id'))
+    work_db.add_work_item(WorkItem("path", "operator", 0, (0, 0), (0, 1), "job_id"))
 
     work_db.set_result(
-        'job_id',
+        "job_id",
         WorkResult(
-            output='first result',
-            test_outcome=TestOutcome.KILLED,
-            worker_outcome=WorkerOutcome.NORMAL,
-            diff='diff'))
+            output="first result", test_outcome=TOutcome.KILLED, worker_outcome=WorkerOutcome.NORMAL, diff="diff"
+        ),
+    )
 
     work_db.set_result(
-        'job_id',
+        "job_id",
         WorkResult(
-            output='second result',
-            test_outcome=TestOutcome.KILLED,
-            worker_outcome=WorkerOutcome.NORMAL,
-            diff='diff'))
+            output="second result", test_outcome=TOutcome.KILLED, worker_outcome=WorkerOutcome.NORMAL, diff="diff"
+        ),
+    )
 
-    results = [r for job_id, r in work_db.results if job_id == 'job_id']
+    results = [r for job_id, r in work_db.results if job_id == "job_id"]
     assert len(results) == 1
-    assert results[0].output == 'second result'
+    assert results[0].output == "second result"
 
 
 def test_num_work_items(work_db):
     count = 10
     for idx in range(count):
-        work_db.add_work_item(
-            WorkItem('path', 'operator', 0, (0, 0), (0, 1),
-                     'job_id_{}'.format(idx)))
+        work_db.add_work_item(WorkItem("path", "operator", 0, (0, 0), (0, 1), "job_id_{}".format(idx)))
     assert work_db.num_work_items == count
 
 
 def test_clear_removes_work_items(work_db):
     for idx in range(10):
-        work_db.add_work_item(
-            WorkItem('path', 'operator', 0, (0, 0), (0, 1),
-                     'job_id_{}'.format(idx)))
+        work_db.add_work_item(WorkItem("path", "operator", 0, (0, 0), (0, 1), "job_id_{}".format(idx)))
     work_db.clear()
     assert work_db.num_work_items == 0
 
 
 def test_clear_work_items_removes_results(work_db):
     for idx in range(10):
-        work_db.add_work_item(
-            WorkItem('path', 'operator', 0, (0, 0), (0, 1),
-                     'job_id_{}'.format(idx)))
-        work_db.set_result('job_id_{}'.format(idx),
-                           WorkResult(WorkerOutcome.NORMAL))
+        work_db.add_work_item(WorkItem("path", "operator", 0, (0, 0), (0, 1), "job_id_{}".format(idx)))
+        work_db.set_result("job_id_{}".format(idx), WorkResult(WorkerOutcome.NORMAL))
 
     work_db.clear()
     assert work_db.num_results == 0
@@ -107,8 +91,9 @@ def test_clear_work_items_removes_results(work_db):
 
 def test_work_items(work_db):
     original = [
-        WorkItem('path_{}'.format(idx), 'operator_{}'.format(idx), idx,
-                 (idx, idx), (idx, idx + 1), 'job_id_{}'.format(idx))
+        WorkItem(
+            "path_{}".format(idx), "operator_{}".format(idx), idx, (idx, idx), (idx, idx + 1), "job_id_{}".format(idx)
+        )
         for idx in range(10)
     ]
     for item in original:
@@ -122,15 +107,28 @@ def test_work_items(work_db):
 def test_results(work_db):
     for idx in range(10):
         work_db.add_work_item(
-            WorkItem('path_{}'.format(idx), 'operator_{}'.format(idx), idx,
-                     (idx, idx), (idx, idx + 1), 'job_id_{}'.format(idx)))
+            WorkItem(
+                "path_{}".format(idx),
+                "operator_{}".format(idx),
+                idx,
+                (idx, idx),
+                (idx, idx + 1),
+                "job_id_{}".format(idx),
+            )
+        )
 
-    original = [('job_id_{}'.format(idx),
-                 WorkResult(
-                     output='data_{}'.format(idx),
-                     test_outcome=TestOutcome.KILLED,
-                     worker_outcome=WorkerOutcome.NORMAL,
-                     diff='diff_{}'.format(idx))) for idx in range(10)]
+    original = [
+        (
+            "job_id_{}".format(idx),
+            WorkResult(
+                output="data_{}".format(idx),
+                test_outcome=TOutcome.KILLED,
+                worker_outcome=WorkerOutcome.NORMAL,
+                diff="diff_{}".format(idx),
+            ),
+        )
+        for idx in range(10)
+    ]
 
     for result in original:
         work_db.set_result(*result)
@@ -142,14 +140,14 @@ def test_results(work_db):
 
 def test_new_work_items_are_pending(work_db):
     items = [
-        WorkItem('path_{}'.format(idx), 'operator_{}'.format(idx), idx,
-                 (idx, idx), (idx, idx + 1), 'job_id_{}'.format(idx))
+        WorkItem(
+            "path_{}".format(idx), "operator_{}".format(idx), idx, (idx, idx), (idx, idx + 1), "job_id_{}".format(idx)
+        )
         for idx in range(10)
     ]
 
     for idx, item in enumerate(items):
-        assert sorted(list(work_db.pending_work_items), key=repr) \
-                == sorted(items[:idx], key=repr)
+        assert sorted(list(work_db.pending_work_items), key=repr) == sorted(items[:idx], key=repr)
         work_db.add_work_item(item)
 
 
@@ -157,8 +155,9 @@ def test_work_items_are_returned_in_random_order(work_db):
     # estimation of survival rate converges much faster if the jobs are
     # executed in random order, this test checks that the order is random
     items = [
-        WorkItem('path_{}'.format(idx), 'operator_{}'.format(idx), idx,
-                 (idx, idx), (idx, idx + 1), 'job_id_{}'.format(idx))
+        WorkItem(
+            "path_{}".format(idx), "operator_{}".format(idx), idx, (idx, idx), (idx, idx + 1), "job_id_{}".format(idx)
+        )
         for idx in range(10)
     ]
     for i in items:
@@ -174,8 +173,9 @@ def test_work_items_are_returned_in_random_order(work_db):
 
 def test_adding_result_clears_pending(work_db):
     items = [
-        WorkItem('path_{}'.format(idx), 'operator_{}'.format(idx), idx,
-                 (idx, idx), (idx, idx + 1), 'job_id_{}'.format(idx))
+        WorkItem(
+            "path_{}".format(idx), "operator_{}".format(idx), idx, (idx, idx), (idx, idx + 1), "job_id_{}".format(idx)
+        )
         for idx in range(10)
     ]
 
@@ -183,21 +183,24 @@ def test_adding_result_clears_pending(work_db):
         work_db.add_work_item(item)
 
     for idx, item in enumerate(items):
-        assert sorted(list(work_db.pending_work_items), key=repr) \
-                == sorted(items[idx:], key=repr)
-        result = ('job_id_{}'.format(idx),
-                  WorkResult(
-                      output='data_{}'.format(idx),
-                      test_outcome=TestOutcome.KILLED,
-                      worker_outcome=WorkerOutcome.NORMAL,
-                      diff='diff_{}'.format(idx)))
+        assert sorted(list(work_db.pending_work_items), key=repr) == sorted(items[idx:], key=repr)
+        result = (
+            "job_id_{}".format(idx),
+            WorkResult(
+                output="data_{}".format(idx),
+                test_outcome=TOutcome.KILLED,
+                worker_outcome=WorkerOutcome.NORMAL,
+                diff="diff_{}".format(idx),
+            ),
+        )
         work_db.set_result(*result)
 
 
 def test_adding_result_completes_work_item(work_db):
     items = [
-        WorkItem('path_{}'.format(idx), 'operator_{}'.format(idx), idx,
-                 (idx, idx), (idx, idx + 1), 'job_id_{}'.format(idx))
+        WorkItem(
+            "path_{}".format(idx), "operator_{}".format(idx), idx, (idx, idx), (idx, idx + 1), "job_id_{}".format(idx)
+        )
         for idx in range(10)
     ]
 
@@ -206,18 +209,21 @@ def test_adding_result_completes_work_item(work_db):
 
     for idx, item in enumerate(items):
         assert [r[0] for r in work_db.completed_work_items] == items[:idx]
-        result = ('job_id_{}'.format(idx),
-                  WorkResult(
-                      output='data_{}'.format(idx),
-                      test_outcome=TestOutcome.KILLED,
-                      worker_outcome=WorkerOutcome.NORMAL,
-                      diff='diff_{}'.format(idx)))
+        result = (
+            "job_id_{}".format(idx),
+            WorkResult(
+                output="data_{}".format(idx),
+                test_outcome=TOutcome.KILLED,
+                worker_outcome=WorkerOutcome.NORMAL,
+                diff="diff_{}".format(idx),
+            ),
+        )
         work_db.set_result(*result)
 
 
 def test_set_config(work_db):
     config = ConfigDict()
-    config['color'] = 'blue'
+    config["color"] = "blue"
     work_db.set_config(config)
 
 
@@ -228,8 +234,8 @@ def test_get_config_raises_ValueError_with_no_config(work_db):
 
 def test_get_config_returns_correct_config(work_db):
     config = ConfigDict()
-    config['color'] = 'blue'
+    config["color"] = "blue"
     work_db.set_config(config)
 
     actual_config = work_db.get_config()
-    assert actual_config['color'] == 'blue'
+    assert actual_config["color"] == "blue"
