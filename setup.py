ef60724077504cf920da01dcda46d63a907c07ae
Travis CI on behalf of Austin Bingham <austin@sixty-north.com>
2021-05-24 12:28:12 +0200

    Bump version: 8.1.1 → 8.1.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="cosmic_ray",
-    version="8.1.1",
+    version="8.1.2",
     packages=find_packages("src"),
     author="Sixty North AS",
     author_email="austin@sixty-north.com",
--- src/cosmic_ray/version.py
+++ src/cosmic_ray/version.py
@@ -1,4 +1,4 @@
 """Cosmic Ray version info."""
 
-__version__ = "8.1.1"
+__version__ = "8.1.2"
 __version_info__ = tuple(__version__.split('.'))
