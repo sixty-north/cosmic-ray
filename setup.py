d8e665cba9416f28292fd952077a11a443822e50
Austin Bingham <austin@sixty-north.com>
2021-08-07 16:51:16 +0200

    Bump version: 8.3.2 → 8.3.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="cosmic_ray",
-    version="8.3.2",
+    version="8.3.3",
     packages=find_packages("src"),
     author="Sixty North AS",
     author_email="austin@sixty-north.com",
