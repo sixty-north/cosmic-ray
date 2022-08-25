8ec6972a663d4b6f4ea14b5cd7792640b94ed93e
Andrew Clark <agclark2@sheffield.ac.uk>
2022-08-25 18:27:09 +0100

    Added a variable inserter mutation operator

---
+++ src/cosmic_ray/operators/variable_inserter.py
@@ -0,0 +1,77 @@
+"""Implementation of the variable-inserter operator."""
+import random
+
+import parso.python.tree
+
+from .operator import Operator
+from parso.python.tree import Name, Number, PythonNode
+from random import randint
+
+class VariableInserter(Operator):
+    """An operator that replaces adds usages of named variables to particular statements."""
+
+    def __init__(self, cause_variable, effect_variable):
+        self.cause_variable = cause_variable
+        self.effect_variable = effect_variable
+
+    def mutation_positions(self, node):
+        """Mutate usages of the specified cause variable. If an effect variable is also
+        specified, then only mutate usages of the cause variable in definitions of the
+        effect variable."""
+        if isinstance(node, PythonNode) and (node.type == "arith_expr" or node.type == "term"):
+            expr_node = node.search_ancestor('expr_stmt')
+            if expr_node:
+                print("VI", expr_node, node)
+                effect_variable_names = [v.value for v in expr_node.get_defined_names()]
+                if self.effect_variable in effect_variable_names:
+                    cause_variables = list(self._get_causes_from_expr_node(expr_node))
+                    if node not in cause_variables:
+                        yield (node.start_pos, node.end_pos)
+
+    def mutate(self, node, index):
+        """Replace cause variable with random constant."""
+        assert isinstance(node, PythonNode)
+        assert (node.type == "arith_expr" or node.type == "term")
+
+        arith_operator = random.choice(['+', '*', '-'])
+        arith_operator_node_start_pos = self._iterate_col(node.end_pos)
+        cause_node_start_pos = self._iterate_col(arith_operator_node_start_pos)
+        arith_operator_node = parso.python.tree.Operator(arith_operator, start_pos=arith_operator_node_start_pos)
+        cause_node = Name(self.cause_variable, start_pos=cause_node_start_pos)
+        replacement_node = parso.python.tree.PythonNode("arith_expr", [node, arith_operator_node, cause_node])
+        return replacement_node
+
+    def _get_causes_from_expr_node(self, expr_node):
+        rhs = expr_node.get_rhs().children
+        return self._flatten_expr(rhs)
+
+    def _flatten_expr(self, expr):
+        for item in expr:
+            # Convert PythonNode to list of its children
+            try:
+                item_to_flatten = item.children
+            except AttributeError:
+                item_to_flatten = item
+            #
+            try:
+                yield from self._flatten_expr(item_to_flatten)
+            except TypeError:
+                yield item_to_flatten
+
+    @staticmethod
+    def _iterate_col(position_tuple):
+        return tuple(sum(x) for x in zip(position_tuple, (0, 1)))
+
+    @classmethod
+    def examples(cls):
+        return (
+            # for cause_variable='x'
+            ('y = x + z', 'y = 10 + z'),
+            # for cause_variable='x' and effect_variable='y'
+            ('j = x + z\ny = x + z', 'j = x + z\ny = -2 + z'),
+            # for cause_variable='x' and effect_variable='j',
+            ('j = x + z\ny = x + z', 'j = 1 + z\ny = x + z'),
+            # for cause_variable='x'
+            ('y = 2*x + 10 + j + x**2', 'y=2*10 + 10 + j + -4**2'),
+        )
+
