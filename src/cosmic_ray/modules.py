78ec4ef1a50f6f1733ef15d71e220e7125ab2a47
Nicolas Ganz <nicolas@keltec.ch>
2022-12-26 13:46:41 +0100

    Feature - Allow module-path configuration to contain multiple paths (#533)
    Without this patch, the module-path in the configuration files can only be exactly one file or directory. This introduces the option to provide a list of directories and files to be able to mutate specific files and directories at different locations.

--- src/cosmic_ray/modules.py
+++ src/cosmic_ray/modules.py
@@ -4,21 +4,22 @@ import glob
 from pathlib import Path
 
 
-def find_modules(module_path):
+def find_modules(module_paths):
     """Find all modules in the module (possibly package) represented by ``module_path``.
 
     Args:
-        module_path: A pathlib.Path to a Python package or module.
+        module_paths: A list of pathlib.Path to Python packages or modules.
 
     Returns:
         An iterable of paths Python modules (i.e. \\*py files).
     """
-    if module_path.is_file():
-        if module_path.suffix == ".py":
-            yield module_path
-    elif module_path.is_dir():
-        pyfiles = glob.glob("{}/**/*.py".format(module_path), recursive=True)
-        yield from (Path(pyfile) for pyfile in pyfiles)
+    for module_path in module_paths:
+        if module_path.is_file():
+            if module_path.suffix == ".py":
+                yield module_path
+        elif module_path.is_dir():
+            pyfiles = glob.glob("{}/**/*.py".format(module_path), recursive=True)
+            yield from (Path(pyfile) for pyfile in pyfiles)
 
 
 def filter_paths(paths, excluded_paths):
