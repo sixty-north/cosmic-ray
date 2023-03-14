b04db33470a491c4375c4ee96b4b5eac6f4c8dba
Austin Bingham <austin@sixty-north.com>
2023-03-14 10:31:21 +0100

    Added 'release' nox command.

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,4 @@
+import argparse
 import nox
 
 nox.options.sessions = ["tests-3.11"]
@@ -15,3 +16,44 @@ def lint(session):
     session.install(".[dev]")
     session.run(*("flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics".split()))
     session.run(*("flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics".split()))
+
+
+@nox.session
+def release(session: nox.Session) -> None:
+    """
+    Kicks off an automated release process by creating and pushing a new tag.
+
+    Invokes bump2version with the posarg setting the version.
+
+    Usage:
+    $ nox -s release -- [major|minor|patch]
+    """
+    parser = argparse.ArgumentParser(description="Release a semver version.")
+    parser.add_argument(
+        "version",
+        type=str,
+        nargs=1,
+        help="The type of semver release to make.",
+        choices={"major", "minor", "patch"},
+    )
+    args: argparse.Namespace = parser.parse_args(args=session.posargs)
+    version: str = args.version.pop()
+
+    # If we get here, we should be good to go
+    # Let's do a final check for safety
+    confirm = input(
+        f"You are about to bump the {version!r} version. Are you sure? [y/n]: "
+    )
+
+    # Abort on anything other than 'y'
+    if confirm.lower().strip() != "y":
+        session.error(f"You said no when prompted to bump the {version!r} version.")
+
+    session.install("bump2version")
+
+    session.log(f"Bumping the {version!r} version")
+    session.run("bump2version", version)
+
+    session.log("Pushing the new tag")
+    session.run("git", "push", external=True)
+    session.run("git", "push", "--tags", external=True)
