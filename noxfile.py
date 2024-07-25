2f55f9ff0d7ff41cc7311e3139f267fb9b87b526
Austin Bingham <austin@sixty-north.com>
2024-07-25 08:43:23 +0200

    Nox now using requirements-dev.lock to get deps.

--- noxfile.py
+++ noxfile.py
@@ -1,19 +1,18 @@
-import argparse
 import nox
 
 nox.options.sessions = ["tests-3.11"]
 
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12"])
-def tests(session):
-    session.install(".[test]")
+def tests(session: nox.Session):
+    session.install("-r", "requirements-dev.lock")
     command = ["pytest", "tests"] + list(session.posargs)
     session.run(*command)
 
 
 @nox.session
 def lint(session):
-    session.install(".[dev]")
+    session.install("-r", "requirements-dev.lock")
 
     session.log("Checking syntax with flake8")
     check_syntax = "python -m flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics"
@@ -22,44 +21,3 @@ def lint(session):
     session.log("Checking style with flake8")
     check_style = "python -m flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics"
     session.run(*check_style.split())
-
-
-@nox.session
-def release(session: nox.Session) -> None:
-    """
-    Kicks off an automated release process by creating and pushing a new tag.
-
-    Invokes bump2version with the posarg setting the version.
-
-    Usage:
-    $ nox -s release -- [major|minor|patch]
-    """
-    parser = argparse.ArgumentParser(description="Release a semver version.")
-    parser.add_argument(
-        "version",
-        type=str,
-        nargs=1,
-        help="The type of semver release to make.",
-        choices={"major", "minor", "patch"},
-    )
-    args: argparse.Namespace = parser.parse_args(args=session.posargs)
-    version: str = args.version.pop()
-
-    # If we get here, we should be good to go
-    # Let's do a final check for safety
-    confirm = input(
-        f"You are about to bump the {version!r} version. Are you sure? [y/n]: "
-    )
-
-    # Abort on anything other than 'y'
-    if confirm.lower().strip() != "y":
-        session.error(f"You said no when prompted to bump the {version!r} version.")
-
-    session.install("bump2version")
-
-    session.log(f"Bumping the {version!r} version")
-    session.run("bump2version", version)
-
-    session.log("Pushing the new tag")
-    session.run("git", "push", external=True)
-    session.run("git", "push", "--tags", external=True)
