3e66b1a2d0ccb2f43f62672697328127234d3f25
Austin Bingham <austin.binghm@pm.me>
2025-09-21 11:48:43 +0200

    xfail of git-filter tests are now non-strict.
    This is because they do pass on some branches (i.e. master) but not others.

--- tests/tools/test_filter_git.py
+++ tests/tools/test_filter_git.py
@@ -6,21 +6,20 @@ import pytest
 
 # Skip these tests when running in GitHub Actions where a proper git
 # workspace isn't available in the test environment.
-xfail_in_ci = pytest.mark.xfail(
+skip_in_ci = pytest.mark.xfail(
     os.environ.get("GITHUB_ACTIONS") == "true",
-    reason="Missing git branches in CI makes these tests unrunnable. Fix this!",
-    strict=True,
+    reason="This test failes on non-master branches in CI, so we ignore this problem for now.",
 )
 
 
-@xfail_in_ci
+@skip_in_ci
 def test_smoke_test_on_initialized_session(initialized_session, fast_tests_root):
     command = [sys.executable, "-m", "cosmic_ray.tools.filters.git", str(initialized_session.session)]
 
     subprocess.check_call(command, cwd=str(fast_tests_root))
 
 
-@xfail_in_ci
+@skip_in_ci
 def test_smoke_test_on_execd_session(execd_session, fast_tests_root):
     command = [sys.executable, "-m", "cosmic_ray.tools.filters.git", str(execd_session.session)]
 
