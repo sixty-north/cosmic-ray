3dbacc1b4eb6d2f1ad079d6e0505182d369ce453
Austin Bingham <austin.binghm@pm.me>
2026-04-02 11:13:20 +0200

    fix(filters): remove noisy pragma-no-mutate stdout spam\n\nRefs: #586

--- tests/tools/test_filter_pragma.py
+++ tests/tools/test_filter_pragma.py
@@ -5,10 +5,12 @@ import sys
 def test_smoke_test_on_initialized_session(initialized_session, fast_tests_root):
     command = [sys.executable, "-m", "cosmic_ray.tools.filters.pragma_no_mutate", str(initialized_session.session)]
 
-    subprocess.check_call(command, cwd=str(fast_tests_root))
+    completed = subprocess.run(command, cwd=str(fast_tests_root), check=True, capture_output=True, text=True)
+    assert completed.stdout == ""
 
 
 def test_smoke_test_on_execd_session(execd_session, fast_tests_root):
     command = [sys.executable, "-m", "cosmic_ray.tools.filters.pragma_no_mutate", str(execd_session.session)]
 
-    subprocess.check_call(command, cwd=str(fast_tests_root))
+    completed = subprocess.run(command, cwd=str(fast_tests_root), check=True, capture_output=True, text=True)
+    assert completed.stdout == ""
